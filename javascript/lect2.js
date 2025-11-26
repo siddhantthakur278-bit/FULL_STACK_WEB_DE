6d4c26630f55e4f9761b22e7960671549b66513f
siddhantthakur278-bit <siddhantthakur278@gmail.com>
2025-11-26 05:44:04 +0530

    done

---
+++ javascript/lect2.js
@@ -0,0 +1,48 @@
+// // var age=25;
+// // if(true){
+// //     console.log(age);
+// // }
+// function solve(){
+//     var age=25;
+//     console.log(age)
+// }
+// console.log(age)
+// solve();
+// var x=10
+// var x=20;
+// console.log(x)
+// {
+//     let age=10
+//     console.log(age)
+// }
+// console.log(age)
+// let age=10
+// console.log(age)
+// let a=20;
+// let a=30;
+// a=45
+// console.log(a)
+// let a =10;
+// a="babbar"
+// a=true
+// a=null
+// console.log(a)
+// const a=28
+// console.log(a)
+// a=20
+// console.log(a)
+// const a=45
+
+// let marks=20.535353;
+// marks="babbar"
+// marks=true
+// let marks;
+// let marks=null;
+// let marks=12345674356789087654345678987654356789765
+// let marks;
+// // marks=false
+// marks=null
+// marks=Symbol("olreno")
+// console.log(marks)
+console.log(typeof(marks))
+
