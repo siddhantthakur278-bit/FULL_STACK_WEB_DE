1b4befb75eed734053b9fa19af2976f96d8fc493
siddhantthakur278-bit <siddhantthakur278@gmail.com>
2026-01-07 23:35:29 +0530

    done

---
+++ javascript/lect4.js
@@ -0,0 +1,183 @@
+// console.log("babbar");
+// console.log("babbar");
+// console.log("babbar");
+// console.log("babbar");
+// console.log("babbar");
+// console.log("babbar");
+// console.log("babbar");
+// console.log("babbar");
+// console.log("babbar");
+// console.log("babbar");
+// console.log("babbar");
+// console.log("babbar");
+// console.log("babbar");
+// console.log("babbar");
+// console.log("babbar");
+// console.log("babbar");
+// console.log("babbar");
+// console.log("babbar");
+// console.log("babbar");
+// console.log("babbar");
+// console.log("babbar");
+// console.log("babbar");
+// console.log("babbar");
+// console.log("babbar");
+// console.log("babbar");
+// console.log("babbar");
+// console.log("babbar");
+// console.log("babbar");
+// console.log("babbar");
+// console.log("babbar");
+// console.log("babbar");
+// console.log("babbar");
+// console.log("babbar");
+// console.log("babbar");
+// console.log("babbar");
+// console.log("babbar");
+// console.log("babbar");
+// console.log("babbar");
+// console.log("babbar");
+// console.log("babbar");
+// console.log("babbar");
+// console.log("babbar");
+// console.log("babbar");
+// console.log("babbar");
+// console.log("babbar");
+// console.log("babbar");
+// console.log("babbar");
+// console.log("babbar");
+// console.log("babbar");
+// console.log("babbar");
+// console.log("babbar");
+// console.log("babbar");
+// console.log("babbar");
+// console.log("babbar");
+// console.log("babbar");
+// console.log("babbar");
+// console.log("babbar");
+// console.log("babbar");
+// console.log("babbar");
+// console.log("babbar");
+// console.log("babbar");
+// console.log("babbar");
+// console.log("babbar");
+// console.log("babbar");
+// console.log("babbar");
+// console.log("babbar");
+// console.log("babbar");
+// console.log("babbar");
+// console.log("babbar");
+// console.log("babbar");
+// console.log("babbar");
+// console.log("babbar");
+// console.log("babbar");
+// console.log("babbar");
+// console.log("babbar");
+// console.log("babbar");
+// console.log("babbar");
+// console.log("babbar");
+// console.log("babbar");
+// console.log("babbar");
+// console.log("babbar");
+// console.log("babbar");
+// console.log("babbar");
+// console.log("babbar");
+for(let i=1;i<=10;i++){
+    console.log("babbar")
+}
+
+for(let i=1;i<=10;i++){
+    console.log(i)
+}
+
+for(let i=10;i>0;i--){
+    console.log(i)
+}
+
+
+for(let i=1;i<=6;i++){
+    if(i==4){
+        break;
+    }
+    else{
+        console.log(i)
+    }
+}
+
+
+for(let i=1;i<=4;i++){
+    if(i==3){
+        continue
+    }
+    else{
+        console.log(i)
+    }
+}
+
+let i=1;
+while(i<6){
+    console.log(i);
+    i++;
+}
+
+let a=1
+while(a<5){
+    console.log("inside the loop");
+    if (a==3){
+        a++;
+        continue;
+        }
+    else{
+        console.log(a);
+    }
+    a++;
+}
+
+let j=1;
+do{
+    console.log("babbar")
+    j++
+}while(j<=10)
+
+let k=1
+do{
+    console.log(k)
+    k++
+
+}while(k<=10);
+
+
+let firstName="Love"
+let lastName="babbar"
+let name=`Love
+Babbar`
+
+console.log(name)
+
+
+let fullName=new String("love babbar")
+console.log(fullName)
+
+
+let op1="english "
+let op2="hindi "
+
+let ans=op1+op2
+console.log(ans)
+
+let finalAnswer=`the final answer is : ${op1}${op2}`
+
+console.log(finalAnswer.length)
+
+console.log(op1.toUpperCase())
+console.log(op2.toLowerCase())
+
+let string="Babbar"
+
+console.log(string.substring(2))
+console.log(string.substring(2,4))
+
+let sentence="hello \\jee \\kaise \\ho \\saare";
+let words=sentence.split(" \\")
+
+console.log(words)
